24221aa47b3cd4eacb7bcbfb4a44f7607432b4d8
PradipJaysawal <gabbarsamba948@gmail.com>
2025-04-08 19:45:30 +0545

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LICT Ecommerce</title>
+    <title>LION Ecommerce</title>
     <link rel="icon" href="{{asset('images/lion.png')}}" type="image/x-icon">
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
